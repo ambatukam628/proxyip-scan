c91da8072bd36212fd89d26f7cb1a0df7e50abc6
bexgempro-sketch <bexgempro@gmail.com>
2026-09-22 18:52:21 +0700

    Update check.js

--- check.js
+++ check.js
@@ -163,8 +163,6 @@ if (cluster.isPrimary) {
                         activeProxies.push(msg.data);
                      //   fs.writeFileSync(jsonOutputFile, JSON.stringify(activeProxies, null, 2))
                     }
-                    process.stdout.clearLine(0)
-                    process.stdout.cursorTo(0)
                     log(`Found: ${color.cyan(msg.data.proxy)}:${color.cyan(msg.data.port)} | ${color.yellow(msg.data.country || 'UNK')} | ${color.blue((msg.data.asOrganization || 'Unknown').substring(0, 30))} | ${color.cyan(msg.data.latency + 'ms')}`, "success")
                     updateProgress()
                 } else if (msg.type === "proxyChecked") {
